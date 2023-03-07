2548e4be989c76f6019c2192da6e67586ed9e3f5
Daniel Krall <63867211+danielkrall98@users.noreply.github.com>
2023-03-08 00:01:42 +0100

    20230308 upload a8

---
+++ A8/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"mvc","c":"Main","l":"bookManager"},{"p":"mvc","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"mvc","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
